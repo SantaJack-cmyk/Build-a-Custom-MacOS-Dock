a57dd2d927bc71014fc67902b9a2c10c8638e4ca
SantaJack-cmyk <183860647+SantaJack-cmyk@users.noreply.github.com>
2024-10-21 12:42:11 -0700

    Update A_Delete Dockscrap.sh

--- A_Delete Dockscrap.sh
+++ A_Delete Dockscrap.sh
@@ -1,13 +1,8 @@
 ﻿#!/bin/bash
 
-#Set Variable for Current User
-currentuser=$(/bin/ls -la /dev/console | /usr/bin/cut -d ' ' -f 4)
-echo currentuser-variable = $currentuser
+#set's the variable for the dockscrap file
 
-#Check to see if Dock has been built already
-dockscrap=/Users/$currentuser/dockscrap.txt
-echo "The dockscrap file is set to" $dockscrap
-
+dockscrap=/Users/Shared/DockFile/dockscrap.txt
 if [ -f $dockscrap ]; then
     echo "The dockscrap file exists. Deleting the file." 
     rm $dockscrap
