f261de4d112cff840f494446330dc17c1d87f9c9
SantaJack-cmyk <183860647+SantaJack-cmyk@users.noreply.github.com>
2024-10-21 12:43:55 -0700

    Update BuildtheDock.sh

--- BuildtheDock.sh
+++ BuildtheDock.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-#Make time for other stuff to finish
-/bin/sleep 3
+mkdir /Users/Shared/DockFile
+chflags hidden /Users/Shared/DockFile
+touch /Users/Shared/DockFile/dockscrap.txt
+chmod ugo+rwx /Users/Shared/DockFile/dockscrap.txt
 
-# Set Variable for Dockutil Binary
+
+OUTFILE="/Library/Scripts/BuildtheDock.sh"
+(
+cat <<'EOF'
+!/bin/bash
+
+#Make sure to name script BuildtheDock.sh in order for it to run correctly. 
+/bin/sleep 2
+
+#Set Variable for DockUtil Binary
 DOCKUTIL_BINARY=/usr/local/bin/dockutil
 
-#Set Variable for Sleep
-sleep=/bin/sleep
-
 #Set Variable for Current User
-currentuser=$(/bin/ls -la /dev/console | /usr/bin/cut -d ' ' -f 4)
+currentuser=$USER
 
 #Create a log file of this script
-exec > /Users/$currentuser/docklog.txt 2>&1
+exec > /Users/$currentuser/Library/docklog.txt 2>&1
 
 #Log the Variables
-echo dockutil-variable = $DOCKUTIL_BINARY
-echo sleep-variable = $sleep
 echo currentuser-variable = $currentuser
+echo dockutil-variable = $DOCKUTIL_BINARY
 
 ############################################################
 
 #Check to see if Dock has been built already
-dockscrap=/Users/$currentuser/dockscrap.txt
+dockscrap=/Users/Shared/DockFile/dockscrap.txt
 echo "The dockscrap file is set to" $dockscrap
-
-if [ -f $dockscrap ]; then
-    echo "The dockscrap file exists. Exiting." 
-    exit 0
-fi
-
-echo No dockscrap found. Continuing to build the dock!
+grep -q -F "$USER" "/Users/Shared/DockFile/dockscrap.txt" && echo 'User Found, Exiting' && exit 0 || echo 'User Not Found, Continue to Build the Dock!'
 
 ############################################################
 
@@ -39,19 +40,24 @@ echo No dockscrap found. Continuing to build the dock!
 echo Removing all Dock Items
 $DOCKUTIL_BINARY --remove all --no-restart
 
-#sleep for 2 seconds
-$sleep 2
-
 #Build the Dock
 $DOCKUTIL_BINARY --add '/System/Applications/Launchpad.app' --allhomes --no-restart
-$DOCKUTIL_BINARY --add '/System/Applications/System Settings.app' --allhomes --no-restart
-$DOCKUTIL_BINARY --add '/Applications/Self Service.app' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/Google Chrome.app/' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/Firefox.app/' --allhomes --no-restart
 $DOCKUTIL_BINARY --add '/System/Cryptexes/App/System/Applications/Safari.app' --allhomes --no-restart
-$DOCKUTIL_BINARY --add '/Applications/Firefox.app' --allhomes --no-restart
-$DOCKUTIL_BINARY --add '/Applications/Google Chrome.app' --allhomes --no-restart
-$DOCKUTIL_BINARY --add '/Library/Application Support/Dock Icons/Office 365.webloc' --label 'Office 365' --no-restart
+$DOCKUTIL_BINARY --add '/Applications/OneDrive.app/' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/Microsoft Outlook.app/' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/Microsoft Word.app/' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/Microsoft Excel.app/' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/Microsoft PowerPoint.app/' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/zoom.us.app/' --allhomes --no-restart
+$DOCKUTIL_BINARY --add '/Applications/Utilities/Adobe Creative Cloud/ACC/Creative Cloud.app/' --allhomes --no-restart
 $DOCKUTIL_BINARY --add '~/Downloads' --view fan --display stack
 echo Added Downloads-Fan View-Stack Display
 
-#Create the dockscrap file
-touch /Users/$currentuser/dockscrap.txt
+
+#Create the dockscrap file and write the current username to the file
+
+echo "$USER" >> /Users/Shared/DockFile/dockscrap.txt
+EOF
+) > "$OUTFILE"
